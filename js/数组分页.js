372c21cf4c9c936d47f59c4063a9121e3cc5b8f0
= <do_web@163.com>
2016-02-16 11:16:36 +0800

    数组分页

---
+++ js/数组分页.js
@@ -0,0 +1,10 @@
+/**
+ * Created by zhanghongtao on 2016/2/16.
+ */
+
+var arr = [];
+for(var i = 1; i<= 70; i++){
+    arr.push(i);
+}
+console.log(arr);
+console.log('---------------------------');
