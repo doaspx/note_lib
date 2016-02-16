a8806845317085c8c80c3d04568d71761bf88376
= <do_web@163.com>
2016-02-16 15:10:43 +0800

    数组分页

--- js/数组分页.js
+++ js/数组分页.js
@@ -1,6 +1,8 @@
 /**
  * Created by zhanghongtao on 2016/2/16.
  */
+TODO: slice： 方法
+
 
 var arr = [];
 for(var i = 1; i<= 70; i++){
@@ -8,3 +10,10 @@ for(var i = 1; i<= 70; i++){
 }
 console.log(arr);
 console.log('---------------------------');
+
+
+var c = arr.slice(0, 50);
+console.log(c);
+
+var c = arr.slice(50, 70);
+console.log(c);
